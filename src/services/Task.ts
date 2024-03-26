087c4d050980d86f7ed2907970202b7cc87add99
SebaSubi <sebaperezlavooy@gmail.com>
2024-03-26 17:33:20 -0300

    Added content to tasks.ts

--- src/services/Task.ts
+++ src/services/Task.ts
@@ -1,14 +1,35 @@
 type Task = {
-    id: string;
-    title: string;
-    compleated: boolean;
+    id: string,
+    description: string,
+    completed: boolean
 }
-let tasks: Task[] = []
+let taskList: Task[] = []
+
 export async function getTasks() {
-    return tasks
+    return taskList
 }
-export async function createTask() { }
 
-export async function deleteCOmpleatedTasks() { }
+export async function createTasks(description: string) {
+    const id = Math.random().toString(36).substring(7);
+    const task: Task = {
+        id,
+        description,
+        completed: false
+    }
+    taskList.push(task)
+    return task
+}
 
+export async function deleteCompletedTasks() {
+    taskList = taskList.filter(element => !element.completed) //filter iterates over eache 'element' and returns a new array, it returs true, if element.completed == false. It takes out anything that isnt like that
+    return taskList
+}
 
+export async function toggleTask(id: string) {
+    const task = taskList.find(element => element.id === id)
+    if(!task){
+        return
+    }
+    task.completed = !task.completed
+    return task
+}
