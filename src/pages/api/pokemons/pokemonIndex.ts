715039921113b9698bdcffa661fd4a43cbd88198
SebaSubi <sebaperezlavooy@gmail.com>
2024-03-27 15:13:44 -0300

    Created PokeDex Basics

---
+++ src/pages/api/pokemons/pokemonIndex.ts
@@ -0,0 +1,13 @@
+
+
+
+export async function getPokemonsApi() {
+    try {
+        const url = `https://pokeapi.co/api/v2/pokemon?limit=10&offset=0`
+        const response = await fetch(url);
+        const result = await response.json();
+        return result
+    } catch (error) {
+        throw error
+    }
+} 
