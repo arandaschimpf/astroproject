e80801541a44f4ddb6c8f099c37f660b5718545c
LukySand <luky.sand@gmail.com>
2024-03-27 16:33:08 -0300

    added new card component

--- src/pages/api/pokemons/pokemonIndex.ts
+++ src/pages/api/pokemons/pokemonIndex.ts
@@ -10,4 +10,15 @@ export async function getPokemonsApi() {
     } catch (error) {
         throw error
     }
-} 
+}
+
+export async function getPokemonByUrl(pokemonUrl: string) {
+    try {
+        const url = pokemonUrl;
+        const response = await fetch(url);
+        const result = await response.json();
+        return result
+    } catch (error) {
+        throw error
+    }
+}
